99037b0fb6d25501116f3eacd53088339c94cc6c
Oren Ghelerter <orenghe@gmail.com>
2022-08-25 21:01:32 +0300

    add header logo and languages
    add header logo and text, header is using svelte. added page /en for english, the regular index page is now with dir=rtl and lang=he. (/)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,7 +1,72 @@
 <script>
-  import "../styles/Header.css";
+  import { isDark } from "../states/darkmode";
+  export let dir;
 </script>
 
-<div class="header-wrapper">
-  <div class="header" />
+<div class="header-wrapper" style={$isDark ? "color: white" : ""}>
+  <div class="header">
+    <div class={dir === "ltr" ? "logo-wrapper" : "logo-wrapper rtl-flex"}>
+      <a href="/" class="logo-link">
+        <img
+          src={$isDark
+            ? "/assets/square-margin-fully-transparent.png"
+            : "/assets/square-margin.png"}
+          alt="header logo"
+          class="header-logo"
+        />
+      </a>
+      <a href="/" class="logo-link">
+        <span class="logo-text">ReverseProof</span>
+      </a>
+    </div>
+  </div>
 </div>
+
+<style>
+  .header-wrapper {
+    position: absolute;
+    top: 20px;
+    width: 80vw;
+    height: 50px;
+    z-index: 100;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+  }
+  .header {
+    position: sticky;
+    width: 100%;
+    height: 100%;
+    border: 1px dashed teal;
+    border-radius: 10px;
+    display: flex;
+    justify-content: space-around;
+  }
+  .logo-wrapper {
+    display: inline-flex;
+  }
+  .rtl-flex {
+    flex-direction: row-reverse;
+  }
+  .logo-wrapper > a {
+    margin: auto;
+    padding: 5px;
+    font-family: "Miriam Libre", sans-serif;
+    font-size: 20px;
+    text-decoration: none;
+    color: inherit;
+    display: inline-flex;
+    justify-content: center;
+    flex-basis: auto;
+  }
+  .logo-text {
+    display: inline-flex;
+    flex-direction: column;
+    vertical-align: middle;
+    line-height: normal;
+  }
+  .header-logo {
+    height: 40px;
+  }
+</style>
